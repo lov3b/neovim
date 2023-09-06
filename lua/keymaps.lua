43e793f10f2b3e13bdac1abd44bcc37f7b3889e6
Love Billenius <lovebillenius@disroot.org>
2023-09-06 13:33:37 +0200

    try to fix telescope problem

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -44,21 +44,6 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   pattern = '*',
 })
 
--- [[ Configure Telescope ]]
--- See `:help telescope` and `:help telescope.setup()`
-require('telescope').setup {
-  defaults = {
-    mappings = {
-      i = {
-        ['<C-u>'] = false,
-        ['<C-d>'] = false,
-      },
-    },
-  },
-}
-
--- Enable telescope fzf native, if installed
-pcall(require('telescope').load_extension, 'fzf')
 
 -- See `:help telescope.builtin`
 vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
--- lua/lazy.lua
+++ lua/lazy.lua
@@ -159,3 +159,18 @@ require('lazy').setup({
   require 'autoformat',
   -- require 'kickstart.plugins.debug',
 }, {})
+
+-- Telescope
+require('telescope').setup {
+  defaults = {
+    mappings = {
+      i = {
+        ['<C-u>'] = false,
+        ['<C-d>'] = false,
+      },
+    },
+  },
+}
+
+-- Enable telescope fzf native, if installed
+pcall(require('telescope').load_extension, 'fzf')
