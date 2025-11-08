b2a51e1b061332b5a211402581e291ccd9efc35c
Love Billenius <lovebillenius@disroot.org>
2025-11-08 11:01:28 +0100

    comments for the plugins

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -1,35 +1,56 @@
 vim.pack.add({
-	"https://github.com/echasnovski/mini.icons",
-	"https://github.com/nvim-lua/plenary.nvim",
-	"https://github.com/nvim-lua/popup.nvim",
+	-- Icons & theming
+	"https://github.com/echasnovski/mini.icons", -- icon provider (used by alpha, lualine, neo-tree)
 	{ src = "https://github.com/catppuccin/nvim", name = "catppuccin" },
-	"https://github.com/ahmedkhalf/project.nvim",
-	"https://github.com/nvim-telescope/telescope.nvim",
-	"https://github.com/nvim-telescope/telescope-fzf-native.nvim",
-	"https://github.com/nvim-treesitter/nvim-treesitter",
-	"https://github.com/nvim-treesitter/nvim-treesitter-textobjects",
-	"https://github.com/williamboman/mason.nvim",
-	"https://github.com/williamboman/mason-lspconfig.nvim",
-	"https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim",
-	"https://github.com/neovim/nvim-lspconfig",
-	"https://github.com/hrsh7th/nvim-cmp",
-	"https://github.com/hrsh7th/cmp-nvim-lsp",
-	"https://github.com/L3MON4D3/LuaSnip",
-	"https://github.com/windwp/nvim-autopairs",
-	"https://github.com/numToStr/Comment.nvim",
-	"https://github.com/lewis6991/gitsigns.nvim",
-	"https://github.com/folke/which-key.nvim",
-	"https://github.com/nvim-lualine/lualine.nvim",
-	"https://github.com/goolord/alpha-nvim",
-	"https://github.com/stevearc/oil.nvim",
-	"https://github.com/nvimtools/none-ls.nvim",
 
-	"https://github.com/MunifTanjim/nui.nvim",
-	"https://github.com/nvim-neo-tree/neo-tree.nvim",
-	"https://github.com/folke/lazydev.nvim", -- For nvim types
+	-- Lua utility libs (dependencies for many plugins)
+	"https://github.com/nvim-lua/plenary.nvim", -- dependency for: telescope, gitsigns, neo-tree, none-ls, project.nvim...
+	"https://github.com/nvim-lua/popup.nvim", -- legacy popup helper; kept for compatibility
 
-	"https://github.com/tpope/vim-fugitive",
-	-- 'https://github.com/ThePrimeagen/harpoon',
+	-- Project management
+	"https://github.com/ahmedkhalf/project.nvim", -- project root detection; dependency of the Telescope “projects” extension
+
+	-- Fuzzy finding
+	"https://github.com/nvim-telescope/telescope.nvim", -- fuzzy finder
+	"https://github.com/nvim-telescope/telescope-fzf-native.nvim", -- native sorter for telescope (needs `make`)
+
+	-- Treesitter
+	"https://github.com/nvim-treesitter/nvim-treesitter", -- syntax tree engine
+	"https://github.com/nvim-treesitter/nvim-treesitter-textobjects", -- depends on nvim-treesitter; extra textobjects
+
+	-- LSP, tools & completion
+	"https://github.com/williamboman/mason.nvim", -- tool installer; dependency of mason-lspconfig and mason-tool-installer
+	"https://github.com/williamboman/mason-lspconfig.nvim", -- bridges mason ↔ nvim-lspconfig
+	"https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim", -- auto-installs formatters/linters; depends on mason.nvim
+	"https://github.com/neovim/nvim-lspconfig", -- LSP client configs; used by mason_lsp setup
+	"https://github.com/hrsh7th/nvim-cmp", -- completion engine; dependency for cmp-nvim-lsp and lazydev’s cmp source
+	"https://github.com/hrsh7th/cmp-nvim-lsp", -- LSP completion source; depends on nvim-cmp
+	"https://github.com/L3MON4D3/LuaSnip", -- snippet engine; used as nvim-cmp snippet backend
+
+	-- Editing QoL
+	"https://github.com/windwp/nvim-autopairs", -- auto insert pairs
+	"https://github.com/numToStr/Comment.nvim", -- toggle comments
+
+	-- Git
+	"https://github.com/lewis6991/gitsigns.nvim", -- git signs; depends on plenary.nvim
+	"https://github.com/tpope/vim-fugitive", -- full git porcelain
+
+	-- UI & ergonomics
+	"https://github.com/folke/which-key.nvim", -- keymap hints
+	"https://github.com/nvim-lualine/lualine.nvim", -- statusline; optional icons via mini.icons
+	"https://github.com/goolord/alpha-nvim", -- start screen; uses mini icons provider
+	"https://github.com/stevearc/oil.nvim", -- file manager in-buffer
+	-- "https://github.com/ThePrimeagen/harpoon", -- marks/quick-jump
+
+	-- Formatting/diagnostics via external tools
+	"https://github.com/nvimtools/none-ls.nvim", -- null-ls fork; depends on plenary.nvim
+
+	-- File explorer
+	"https://github.com/MunifTanjim/nui.nvim", -- UI components; dependency of neo-tree
+	"https://github.com/nvim-neo-tree/neo-tree.nvim", -- file explorer; depends on nui.nvim + plenary.nvim + icons
+
+	-- Lua nvim experience
+	"https://github.com/folke/lazydev.nvim", -- improves LuaLS workspace/types; provides cmp source (depends on nvim-cmp if present)
 }, { load = true })
 
 vim.api.nvim_create_autocmd("PackChanged", {
