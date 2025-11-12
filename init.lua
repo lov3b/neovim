76a9cd0d6d7c394f5e65aee358a3756808abe324
Love Billenius <lovebillenius@disroot.org>
2025-11-12 12:05:06 +0100

    Prefer nushell

--- init.lua
+++ init.lua
@@ -3,6 +3,7 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 -- Core
+require("core.shell")
 require("core.plugins")
 require("core.options")
 require("core.keymaps")
---
+++ lua/core/shell.lua
@@ -0,0 +1,12 @@
+# prefer nushell on windows
+if vim.fn.has("win32") == 1 or vim.fn.has("win64") == 1 then
+	local nu_path = vim.fn.expand("~/AppData/Local/Programs/nu/bin/nu.exe")
+	local use_nu = vim.fn.executable("nu") or vim.fn.filereadable(nu_path)
+
+	if use_nu then
+		vim.opt.shell = vim.fn.executable("nu") and "nu" or nu_path
+		vim.opt.shellcmdflag = "-c"
+		vim.opt.shellquote = ""
+		vim.opt.shellxquote = ""
+	end
+end
