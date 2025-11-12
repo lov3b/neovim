2d3363ab363d507bf64840e6dd2af37616bfafc0
Love Billenius <lovebillenius@disroot.org>
2025-11-12 13:02:31 +0100

    Add command to turn off format on save

--- after/plugin/conform.lua
+++ after/plugin/conform.lua
@@ -23,9 +23,28 @@ require("conform").setup({
 		kotlin = { "ktlint" },
 		java = { "google-java-format" },
 	},
+	format_on_save = function(bufnr)
+		if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+			return
+		end
+		return { timeout_ms = 500, lsp_format = "fallback" }
+	end,
+})
 
-	format_on_save = {
-		timeout_ms = 2000,
-		lsp_fallback = true,
-	},
+vim.api.nvim_create_user_command("FormatDisable", function(args)
+	if args.bang then
+		vim.b.disable_autoformat = true
+	else
+		vim.g.disable_autoformat = true
+	end
+end, {
+	desc = "Disable autoformat-on-save",
+	bang = true,
+})
+
+vim.api.nvim_create_user_command("FormatEnable", function()
+	vim.b.disable_autoformat = false
+	vim.g.disable_autoformat = false
+end, {
+	desc = "Re-enable autoformat-on-save",
 })
