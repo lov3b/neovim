1bc86e6373d8c7395c7b672753b8cdb13959ecd0
Love Billenius <lovebillenius@disroot.org>
2025-12-01 20:11:41 +0100

    revert

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -56,7 +56,7 @@ end, { desc = "Yank entire buffer to clipboard" })
 -- Paste to entire buffer
 map("n", "<leader>p", function()
 	local row, col = unpack(vim.api.nvim_win_get_cursor(0))
-	local cb = vim.fn.getreg("+", 1)
+	local cb = vim.fn.getreg("+", 1, true)
 	vim.api.nvim_buf_set_lines(0, 0, -1, false, cb)
 	row = math.min(row, #cb)
 	vim.api.nvim_win_set_cursor(0, { row, col })
