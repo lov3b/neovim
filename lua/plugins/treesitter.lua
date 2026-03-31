bf32945179a34d178fb32772d486ec8a50bc69d5
Love Billenius <lovebillenius@disroot.org>
2026-03-31 15:46:03 +0200

    update

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,7 +5,7 @@ return {
 		"nvim-treesitter/nvim-treesitter-textobjects",
 	},
 	config = function()
-		require("nvim-treesitter.configs").setup({
+		require("nvim-treesitter").setup({
 			ensure_installed = {
 				"nix",
 				"lua",
